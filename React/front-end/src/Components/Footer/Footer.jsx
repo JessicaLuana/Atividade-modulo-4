5bd379dfe0a7fd1d21e50c74ec54f54320f0e54c
JessicaLuana <j.luaana@live.com>
2022-02-14 22:48:15 -0300

    Trabalho Individual

---
+++ React/front-end/src/Components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+export default () => {
+    return(
+        <footer>
+        <div>
+            <p class="rodape ">Desenvolvido por Viajamar &copy; 2022</p>
+        </div>
+      </footer>
+    );
+}
---
+++ React/front-end/src/Components/Main/MainCadastroCliente.jsx
@@ -0,0 +1,15 @@
+import viajamar from "../../assets/img/viajamar.png"
+
+import SectionCadastro from "./Section/SectionCadastro";
+
+export default () => {
+    return (
+        <main className="container-fluid">
+            <div className="">
+                <center><img src={viajamar} className="" /></center>
+            </div>
+
+           <SectionCadastro></SectionCadastro>
+        </main>
+    );
+}
